4e6f000f3f8751f2e195f251875b42129f650926
Farjana <farjanajhn12@gmail.com>
2023-06-14 00:06:21 +0600

    created a api for make user

--- index.js
+++ index.js
@@ -33,11 +33,33 @@ async function run() {
     const cartCollection = client.db("lingoDb").collection("carts");
     
     //users related api
+    app.get('/users',async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result)
+    })
     app.post('/users', async (req, res) => {
       const user = req.body;
+      const query = { email: user.email }
+      const existingUser = await usersCollection.findOne(query);
+      console.log('existingUser',existingUser)
+      if (existingUser) {
+        return res.send({message:'user already exist'})
+      }
       const result = await usersCollection.insertOne(user);
       res.send(result);
       
+    })
+
+    app.patch('user/admin/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role:'admin'
+        }
+      }
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result)
     })
 
     //classes
